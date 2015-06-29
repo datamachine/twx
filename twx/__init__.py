2a45bc69e565d4aaa950c50195c3ea084ccab1a8
Phillip Lopo <philliplopo@gmail.com>
2015-06-29 15:08:28 -0700

    only expand namespace for supported python versions

--- twx/__init__.py
+++ twx/__init__.py
@@ -1,7 +1,15 @@
 import sys
 
 """
+Expand the tex namespace
+"""
+if sys.version_info[0] == 3 and sys.version_info[1] >=2:
+	from pkgutil import extend_path
+	__path__ = extend_path(__path__, __name__)
+
+"""
 Only import twx modules on supported pytohn versions. Type hinting breaks testing 2.7 against twx.botapi
 """
 if sys.version_info[0] == 3 and sys.version_info[1] >=4:
 	from . twx import *
+
